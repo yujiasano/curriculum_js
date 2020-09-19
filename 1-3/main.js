a98f516fe94389b5787360491b2a5cbad4ac784a
yujiasano <forever2011.0903@gmail.com>
2020-09-19 23:55:28 +0900

    条件分岐・演算子　課題

---
+++ 1-3/main.js
@@ -0,0 +1,17 @@
+console.log("a !== 10");
+console.log("b >= 10 && b <= 20 && b%2===0");
+
+let x = 10;
+let y = 0;
+
+if(x >= 10 && x <= 20){
+  console.log("成功です");
+}else{
+  console.log("失敗です");
+}
+
+if(y % 2 === 0){
+  console.log("偶数です");
+}else {
+  console.log("奇数です");
+}
