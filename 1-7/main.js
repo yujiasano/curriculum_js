af99852409f03b070f6a0a3d158f7c2093b05800
yujiasano <forever2011.0903@gmail.com>
2020-09-21 10:56:48 +0900

    クラスとインスタンス 課題

---
+++ 1-7/main.js
@@ -0,0 +1,16 @@
+class Taiyaki {
+  constructor(taste){
+    this.taste = taste;
+  }
+};
+
+let anko = new Taiyaki("あんこ");
+console.log("中身は" + anko.taste + "です");
+
+let cream = new Taiyaki("クリーム");
+console.log("中身は" + cream.taste + "です");
+
+let cheese = new Taiyaki("チーズ");
+console.log("中身は" + cheese.taste + "です");
+
+
