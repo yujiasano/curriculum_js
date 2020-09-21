7b2bd7970e8067b54f0d93ff3d8e118a489a159f
yujiasano <forever2011.0903@gmail.com>
2020-09-21 10:15:06 +0900

    配列・オブジェクト　課題

---
+++ 1-6/main.js
@@ -0,0 +1,17 @@
+// 問１
+let scores = [10, 15, 20, 25];
+
+console.log(scores);
+console.log(scores[0]+"は偶数です");
+console.log(scores[2]+"は偶数です");
+
+// 問２
+let car = {
+  gass: 20.5,
+  num: 1234
+};
+
+console.log(car);
+console.log("ガソリンは" + car.gass + "です");
+console.log("ナンバーは" + car.num + "です");
+
